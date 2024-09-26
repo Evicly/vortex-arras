b6f55c178a3533db01455c290bb76c29eb8accb2
EviclyBoss <155589121+Evicly@users.noreply.github.com>
2024-09-26 14:35:10 +0000

    added vip token

--- server/permissions.js
+++ server/permissions.js
@@ -1,15 +1,21 @@
 module.exports = [
   {
-    key: process.env.HEAD_DEV,
+    key: process.env.DEV,
     nameColor: "#00ff00",
     class: "developer",
     infiniteLevelUp: true,
     administrator: true,
   },
   {
-    key: process.env.BETA_TESTER,
+    key: process.env.STAFF,
     nameColor: "#e540ff",
     class: "staff",
+    infiniteLevelUp: true,
+  },
+  {
+    key: process.env.VIP,
+    nameColor: "#ffb300",
+    class: "vip",
     infiniteLevelUp: true,
   },
   {
